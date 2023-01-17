8fd08306885929ca7c8b23d7ca8104e513b1a7ac
taylrzhang <neil0725@hotmail.com>
2023-01-17 10:04:38 -0800

    save data into db

--- client/components/Keyboard.jsx
+++ client/components/Keyboard.jsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 // look at the value of the input feild
 // check length - 1 of input feild against target string unless length is 0
 
-const Keyboard = ({target, targetString, targetIndex, indexSetter, timer, timerSetter, errors, errorsSetter}) => {
-  const [inputValue, setInputValue] = useState('');
+const Keyboard = ({
+  target,
+  targetString,
+  targetIndex,
+  indexSetter,
+  timer,
+  timerSetter,
+  errors,
+  errorsSetter,
+}) => {
+  const [inputValue, setInputValue] = useState("");
   // let timerStart
 
   // useEffect(() => {
@@ -19,7 +28,6 @@ const Keyboard = ({target, targetString, targetIndex, indexSetter, timer, timerS
   //   }
   // }, [inputValue])
 
-
   // useEffect(() => {
   //   if (inputValue.length !== 0) {
   //     if (inputValue.length < targetIndex) indexSetter(inputValue.length);
@@ -32,12 +40,13 @@ const Keyboard = ({target, targetString, targetIndex, indexSetter, timer, timerS
 
   return (
     <div id="keyboard">
-      <input id='text-input' 
+      <input
+        id="text-input"
         type="text"
         placeholder="Start typing..."
         onKeyDown={(e) => {
           // console.log(e.key)
-          if (e.key !== 'Shift' && e.key !== 'Backspace') {
+          if (e.key !== "Shift" && e.key !== "Backspace") {
             if (e.key === target[targetIndex]) {
               console.log("correct");
               indexSetter(targetIndex + 1);
@@ -47,20 +56,19 @@ const Keyboard = ({target, targetString, targetIndex, indexSetter, timer, timerS
               console.log(errors);
             }
           }
-          setInputValue(e.target.value)
-          console.log(inputValue)
+          setInputValue(e.target.value);
+          console.log(inputValue);
         }}
-
         // onChange={(e) => {
         //   setInputValue(e.target.value)
         // }}
         onClick={() => {
-          timerSetter(Date.now())
+          timerSetter(Date.now());
           console.log("timer Start", timer);
         }}
       />
     </div>
-  )
-}
+  );
+};
 
 export default Keyboard;
