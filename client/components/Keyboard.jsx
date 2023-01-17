30f04a58d0cb41ff842a6ce4bd04a23fdb58a609
Alex Lopez <alopez.56207@gmail.com>
2023-01-16 21:28:36 -0800

    wpm, cpm, and accuracy working

--- client/components/Keyboard.jsx
+++ client/components/Keyboard.jsx
@@ -3,21 +3,21 @@ import React, { useEffect, useState } from 'react';
 // look at the value of the input feild
 // check length - 1 of input feild against target string unless length is 0
 
-const Keyboard = ({target, targetString, targetIndex, indexSetter, timer, timerSetter}) => {
+const Keyboard = ({target, targetString, targetIndex, indexSetter, timer, timerSetter, errors, errorsSetter}) => {
   const [inputValue, setInputValue] = useState('');
   // let timerStart
 
-  useEffect(() => {
-    if (inputValue.length !== 0) {
-      if (inputValue[targetIndex] === target[targetIndex]) {
-        console.log('correct');
-        indexSetter(targetIndex + 1);
-      }
-      else if (inputValue[targetIndex] !== target[targetIndex]) {
-        console.log('wrong');
-      }
-    }
-  }, [inputValue])
+  // useEffect(() => {
+  //   if (inputValue.length !== 0) {
+  //     if (inputValue[targetIndex] === target[targetIndex]) {
+  //       console.log('correct');
+  //       indexSetter(targetIndex + 1);
+  //     }
+  //     else if (inputValue[targetIndex] !== target[targetIndex]) {
+  //       console.log('wrong');
+  //     }
+  //   }
+  // }, [inputValue])
 
 
   // useEffect(() => {
@@ -34,17 +34,25 @@ const Keyboard = ({target, targetString, targetIndex, indexSetter, timer, timerS
     <div id="keyboard">
       <input id='text-input' 
         type="text" 
-        // onKeyDown={(e) => {
-        //   // console.log(e.key)
-        //   if (e.key !== 'Shift' && e.key !== 'Backspace') buffer.push(e.key)
-        //   //if (e.key === 8) buffer.pop();
-        //   // console.log(buffer);
-        //   setInputValue(e.target.value)
-        //   console.log(inputValue)
-        // }}
-        onChange={(e) => {
+        onKeyDown={(e) => {
+          // console.log(e.key)
+          if (e.key !== 'Shift' && e.key !== 'Backspace') {
+            if (e.key === target[targetIndex]) {
+              console.log("correct");
+              indexSetter(targetIndex + 1);
+            } else {
+              console.log("Wrong");
+              errorsSetter(errors + 1);
+              console.log(errors);
+            }
+          }
           setInputValue(e.target.value)
+          console.log(inputValue)
         }}
+
+        // onChange={(e) => {
+        //   setInputValue(e.target.value)
+        // }}
         onClick={() => {
           timerSetter(Date.now())
           console.log("timer Start", timer);
